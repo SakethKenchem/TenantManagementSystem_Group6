79a7044a34696e5bc4ad8b0a3f35d147ea4eb9b9
SakethKenchem <saketh.kenchem@strathmore.edu>
2026-09-03 14:57:52 +0300

    Implemented Parts 0-2 with comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+/*
+Andrew Mutero - 150736
+Saketh Kenchem - 152324
+Murji Viqarhussein - 193642
+Mark Kipruto - 169386
+Lewis Kamunjeru - 155669
+ */
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main() {
+    println("Welcome to the Tenant Management System") //PART 0
+
+    val tenantId: Int = 1001 // tenantID must remain the same
+    //tenantID = 1002
+    val name: String = "Jane Wanjiku" //the name is not being changed
+    val phone: String = "0712345678" // phone number does not change frequently. 0 has no significance and could be lost, we don't perform arithmetic on phone numbers.
+    val houseNumber: String = "A-204" //house number does not change at all
+    val monthlyRent: Int = 25000 //the rent does not change
+
+    var amountPaid: Int = 15000 //tenant's paid balance will change payments in the future
+    val block: Char = 'A'
+    val isActive: Boolean = true
+
+
+    println("Amount paid before: $amountPaid")
+
+    amountPaid = amountPaid + 5000
+
+    println("Amount paid after: $amountPaid")
+
+    val rentAsDouble: Double = monthlyRent.toDouble() //PART 2.3
+    println("Rent as Double: " + rentAsDouble)
+
+    /* PART 1.3- reassigning tenantID to 1002 results in "'val' cannot be reassigned." in the compiler.
+    - the compiler refuses because tenantID has been declared using val, which cannot be reassigned.
+    - val -> var can make the program compile
+     */
+
+    /* PART 2.3- Does not compile because Initializer type mismatch: expected 'Double', actual 'Int'.
+    -Kotlin's explicit conversions ensure strict type safety, avoiding accidental precision loss
+     and unexpected behavior.
+     */
+
+    val registrationNumber: Long = 999_999_999L
+    println("Registration number: $registrationNumber")
+
+
 }
